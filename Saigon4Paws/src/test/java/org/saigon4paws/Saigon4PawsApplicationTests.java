e972578a901b7be40278f3886f74e721871a2c29
Nguyen Son <50987070+SonNVQ@users.noreply.github.com>
2023-08-24 16:42:10 +0700

    S4P-60 Initial project
    +Server port [8888]
    +MariaDB connect at port [3308], database [saigon4paws]

---
+++ Saigon4Paws/src/test/java/org/saigon4paws/Saigon4PawsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.saigon4paws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Saigon4PawsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
