627c8eb5621d3c616609e6dba2f98594433099f8
Nguyen Son <50987070+SonNVQ@users.noreply.github.com>
2023-09-01 12:59:02 +0700

    S4P-62 Fix ArticleCategoryRepository

--- Saigon4Paws/src/main/java/org/saigon4paws/Repositories/ArticleCategoryRepository.java
+++ Saigon4Paws/src/main/java/org/saigon4paws/Repositories/ArticleCategoryRepository.java
@@ -1,8 +1,9 @@
 package org.saigon4paws.Repositories;
 
+import org.saigon4paws.Models.ArticleCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ArticleCategoryRepository extends JpaRepository<ArticleRepository, Integer> {
+public interface ArticleCategoryRepository extends JpaRepository<ArticleCategory, Integer> {
 }
